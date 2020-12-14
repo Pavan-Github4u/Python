66e6a5b684fd8e4750dc0abd18c2cb87c890effa
Pavan-Github4u <75560175+Pavan-Github4u@users.noreply.github.com>
2020-12-14 18:10:09 +0530

    Introduction

---
+++ Introduction.py
@@ -0,0 +1,4 @@
+#Array's are one of the most commonly used Data Structure
+#The elements of an array are stored in contigeous memory locations
+#Arrays are of two types 1) Static 2) Dynamic
+#Static arrays are 
